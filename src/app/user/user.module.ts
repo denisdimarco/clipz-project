fdeebf4be07cdb2b1999a9a493435ee1d6ed84e5
denisdimarco <denisdimarcoleda@gmail.com>
2022-01-21 21:54:29 -0300

    modal component generated

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
-
+import { SharedModule } from "../shared/shared.module";
 
 
 @NgModule({
@@ -12,7 +12,8 @@ import { AuthModalComponent } from './auth-modal/auth-modal.component';
     AuthModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class UserModule { }
